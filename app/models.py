4e9326f7c19024b47a990119d65437850ff65c16
gihon12312340 <gihon12312340@gmail.com>
2023-08-02 10:49:55 +0800

    update Mysql

--- app/models.py
+++ app/models.py
@@ -12,7 +12,7 @@ def load_user(id):
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
-    password = db.Column(db.String(20), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
 
--- config.py
+++ config.py
@@ -3,22 +3,26 @@ from flask import Config
 
 class Config(Config):
 
-    #SECRET KEY
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'A-VERY-LONG-SECRET-KEY'
-    
-    #RECAPTCHA PUBLIC KEY
-    RECAPTCHA_PUBLIC_KEY  = os.environ.get('RECAPTCHA_PUBLIC_KEY') or 'A-VERY-LONG-SECRET-KEY'
-    RECAPTCHA_PRIVATE_KEY = os.environ.get('RECAPTCHA_PRIVATE_KEY') or 'A-VERY-LONG-SECRET-KEY'
 
-    
-    #Database configuration
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') 
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # 設定 Flask 和相關套件
+    SECRET_KEY = 'A-VERY-LONG-SECRET-KEY'
+        
+    # RECAPTCHA 公鑰和私鑰
+    RECAPTCHA_PUBLIC_KEY = 'A-VERY-LONG-SECRET-KEY'
+    RECAPTCHA_PRIVATE_KEY = 'A-VERY-LONG-SECRET-KEY'
 
-    # 設定 Flask-Mail 相關設定
-    MAIL_SERVER   = os.environ.get('MAIL_SERVER')
-    MAIL_PORT     = 587
-    MAIL_USE_TLS  = True
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or'example@gmail.com'  
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or'0000'
+    # 資料庫設定，使用環境變數中的值，若無則使用預設值
+    DATABASE_USER = os.environ.get('DATABASE_USER')
+    DATABASE_PASSWORD = os.environ.get('DATABASE_PASSWORD')
+    DATABASE_HOST = os.environ.get('DATABASE_HOST')
+    DATABASE_PORT = os.environ.get('DATABASE_PORT')
+    DATABASE_NAME = os.environ.get('DATABASE_NAME')
 
+    SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}'
+
+    # Flask-Mail 相關設定
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = 'gihon12312340@gmail.com'
+    MAIL_PASSWORD = 'zrznlnnlxgdclamj'
